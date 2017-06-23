eca74a1d9a118d2c52cac6d49b346dcd2dc9b95c
Aixile <kaguyaelf@gmail.com>
2017-06-23 10:31:44 +0900

    add cramer gan

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Chainer-GANs
 
 Various GANs with Chainer
-### Requirement 
-* Chainer==1.24.0 
+### Requirement
+* Chainer==1.24.0
 * OpenCV
 
 ### List
@@ -11,7 +11,8 @@ Various GANs with Chainer
 * WGAN: [Wasserstein GAN](https://arxiv.org/abs/1701.07875)
 * WGAN-GP: [Improved Training of Wasserstein GANs](https://arxiv.org/abs/1704.00028)
 * DRAGAN: [How to Train Your DRAGAN](https://arxiv.org/abs/1705.07215)
-* [WIP]Alpha-GAN: [Variational Approaches for Auto-Encoding Generative Adversarial Networks](https://arxiv.org/abs/1706.04987)
+* [WIP] CramerGAN: [The Cramer Distance as a Solution to Biased Wasserstein Gradients](The Cramer Distance as a Solution to Biased Wasserstein Gradients)
+* [WIP] αGAN: [Variational Approaches for Auto-Encoding Generative Adversarial Networks](https://arxiv.org/abs/1706.04987)
 
 
 ### Gradient Penalty
@@ -21,11 +22,7 @@ Most of recent GANs (WGAN-GP, CramerGAN, DRAGAN) contains the gradient norm regu
 The current version of Chainer do not support high order derivatives, a solution is to mannually implement the backward procedure with auto-differentiable chainer.functions. (Refer the WGAN-GP codes for the details.)
 
 * L.Linear, L.Convolution2D, L.Deconvolution2D, F.leaky_relu, F.relu, F.sigmoid, F.tanh, L.LayerNormalization is implemented.
-* Some GAN papers suggest to use LayerNormalization instead on BatchNormalization in the discriminator in the case of gradient penalty. 
+* Some GAN papers suggest to use LayerNormalization instead on BatchNormalization in the discriminator in the case of gradient penalty.
 
 
 **Special thanks to [mattya](https://github.com/mattya) for the idea and reference codes.**
-
-
-
-
---
+++ cramergan/updater.py
@@ -0,0 +1,90 @@
+import numpy as np
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import cuda, optimizers, serializers, Variable
+import sys
+sys.path.insert(0, '../')
+from common.loss_functions import *
+
+class Updater(chainer.training.StandardUpdater):
+
+    def __init__(self, *args, **kwargs):
+        self.gen, self.dis = kwargs.pop('models')
+        self._iter = 0
+        params = kwargs.pop('params')
+        self._img_size = params['img_size']
+        self._img_chan = params['img_chan']
+        self._batch_size = params['batch_size']
+        self._latent_len = params['latent_len']
+        self._lambda_gp = params['lambda_gp']
+        super(Updater, self).__init__(*args, **kwargs)
+
+    def get_real_image_batch(self):
+        xp = self.gen.xp
+        batch = self.get_iterator('main').next()
+        t_out = xp.zeros((self._batch_size, self._img_chan, self._img_size, self._img_size)).astype("f")
+        for i in range(self._batch_size):
+            t_out[i, :] = xp.asarray(batch[i])
+        return t_out
+
+    def get_fake_image_batch(self):
+        z = self.get_latent_code_batch()
+        x_out = self.gen(Variable(z, volatile=True), test=True).data
+        return x_out
+
+    def get_latent_code_batch(self):
+        xp = self.gen.xp
+        z_in = xp.random.normal(size=(self._batch_size, self._latent_len)).astype("f")
+        return z_in
+
+    def update_core(self):
+        xp = self.gen.xp
+        self._iter += 1
+
+        opt_g = self.get_optimizer('gen')
+        opt_d = self.get_optimizer('dis')
+
+        data_z0 = self.get_latent_code_batch()
+        x_fake0 = self.gen(Variable(data_z0))
+        data_z1 = self.get_latent_code_batch()
+        x_fake1 = self.gen(Variable(data_z1))
+        data_x = self.get_real_image_batch()
+
+        eta = np.random.rand()
+        x_inter = Variable((data_x * eta + (1.0 - eta) * x_fake0.data).astype('f'))
+
+        dis_x_fake0 = self.dis(x_fake0)
+        dis_x_fake1 = self.dis(x_fake1)
+        dis_x_real = self.dis(x_real)
+
+        loss_gen = loss_l2_sqrt(dis_x_fake0, dis_x_real) + \
+                    loss_l2_sqrt(dis_x_fake1, dis_x_real) - \
+                    loss_l2_sqrt(dis_x_fake0, dis_x_fake1)
+
+        chainer.report({'loss': loss_gen}, self.gen)
+        opt_g.zero_grads()
+        loss_gen.backward()
+        opt_g.update()
+
+        x_fake0.unchain_backward()
+        x_fake1.unchain_backward()
+
+        loss_surrogate = loss_l2_sqrt(dis_x_fake0, dis_x_fake1) - \
+                    loss_l2_sqrt(dis_x_fake0, 0.0) + \
+                    loss_l2_sqrt(dis_x_real, 0.0) - \
+                    loss_l2_sqrt(dis_x_real, dis_x_fake1)
+
+        dis_x_inter = self.dis(x_inter, retain_forward=True)
+        g = xp.ones_like(dis_x_inter.data)
+        grad = self.dis.differentiable_backward(Variable(g))
+        grad_l2 = F.sqrt(F.sum(grad**2, axis=(1, 2, 3)))
+        loss_gp = self._lambda_gp * loss_l2(grad_l2, 1.0)
+
+        loss_dis = loss_surrogate + loss_gp
+
+        opt_d.zero_grads()
+        loss_dis.backward()
+        opt_d.update()
+
+        chainer.report({'loss': loss_dis, 'loss_gp': loss_gp}, self.dis)
